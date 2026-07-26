bfde8a43ce8816f7012046f2ff51e55bf904938f
sergeysikora1996-star <sergeysikora1996-star@users.noreply.github.com>
2026-07-26 22:10:30 +0200

    Зменшено розмір фото і виведено текст на передній план

---
+++ fix_size_zindex.py
@@ -0,0 +1,22 @@
+﻿import re
+
+with open('css/instagram-business-a31ab8.webflow.css', 'r', encoding='utf-8') as f:
+    css = f.read()
+
+css = css.replace(
+    '  .mentor_image {\n    width: 88vw;',
+    '  .mentor_image {\n    width: 75vw;'
+)
+
+css = css.replace(
+    '  .mentor_image.is-size {\n    width: 85vw;',
+    '  .mentor_image.is-size {\n    width: 72vw;'
+)
+
+css = css.replace(
+    '  .mentor_image-wrap {\n    border-radius: 4.44vw;\n    position: absolute;\n    top: auto;\n    bottom: 0%;\n    left: auto;\n    right: -10vw;\n    overflow: hidden;\n  }',
+    '  .mentor_image-wrap {\n    border-radius: 4.44vw;\n    position: absolute;\n    top: auto;\n    bottom: 0%;\n    left: auto;\n    right: -10vw;\n    overflow: hidden;\n    z-index: 1;\n  }'
+)
+
+with open('css/instagram-business-a31ab8.webflow.css', 'w', encoding='utf-8') as f:
+    f.write(css)
---
+++ fix_text_zindex.py
@@ -0,0 +1,2 @@
+﻿with open('css/ukraine-theme.css', 'a', encoding='utf-8') as f:
+    f.write('\n.mentor_card > div:not(.mentor_image-wrap) {\n  position: relative;\n  z-index: 2;\n}\n')
